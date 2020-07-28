a48f163af9a136fb3c38997c2e91772d89d1b092
softwaiter <softwaiter@126.com>
2020-07-28 08:52:23 +0800

    '调整测试案例'

--- UnitTest/UnitTest4.cs
+++ UnitTest/UnitTest4.cs
@@ -30,25 +30,7 @@ namespace UnitTest
             Test4();
             Test5();
             Test6();
-            Test8();
-            //Test7();
-        }
-
-        public void Test8()
-        {
-            int i = 10000;
-            while (i > 0)
-            {
-                dynamic newuser = ModelObject.New("User");
-                newuser.Name = "wangxm" + i;
-                newuser.Age = 18;
-                newuser.Birthday = new DateTime(1980, 6, 14);
-                newuser.Deposit = 10000000.58;
-                newuser.IsAdmin = true;
-                OrmUtils.Model("User").SetValues(newuser).Save();
-
-                i--;
-            }
+            Test7();
         }
 
         [Description("创建User模型的物理表。")]
