69b585d9636af0ea22157931ac6304fcf13d1485
softwaiter <softwaiter@126.com>
2023-05-09 18:41:27 +0800

    :construction:增加JSON类型属性测试用例

---
+++ Test/10-JSONProperty.cs
@@ -0,0 +1,66 @@
+﻿using CodeM.Common.Orm;
+using CodeM.Common.Tools.DynamicObject;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+
+namespace Test
+{
+    [TestClass]
+    public class _10_JSONProperty
+    {
+        [TestInitialize]
+        public void Init()
+        {
+            Derd.RegisterProcessor("EncryptDeposit", "Test.Processors.EncryptDeposit");
+            Derd.RegisterProcessor("DecryptDeposit", "Test.Processors.DecryptDeposit");
+
+            string modelPath = Path.Combine(Environment.CurrentDirectory, "..\\..\\..\\models");
+            Derd.ModelPath = modelPath;
+            Derd.Load();
+
+            Derd.TryCreateTables(true);
+        }
+
+        [TestMethod]
+        public void WriteJsonValue()
+        {
+            dynamic newPlayer = new DynamicObjectExt();
+            newPlayer.Type = 0;
+            newPlayer.Skill = new DynamicObjectExt();
+            newPlayer.Skill.Walk = 1;
+            newPlayer.Skill.Run = 0.8;
+            newPlayer.Skill.Attack = 50;
+            newPlayer.Skill.MagicAttack = 80;
+            newPlayer.Skill.Blood = 100;
+            bool ret = Derd.Model("Player").SetValues(newPlayer).Save();
+            Assert.IsTrue(ret);
+        }
+
+        [TestMethod]
+        public void ReadJsonValue()
+        {
+            dynamic newPlayer = new DynamicObjectExt();
+            newPlayer.Type = 0;
+            newPlayer.Skill = new DynamicObjectExt();
+            newPlayer.Skill.Walk = 1;
+            newPlayer.Skill.Run = 0.8;
+            newPlayer.Skill.Attack = 50;
+            newPlayer.Skill.MagicAttack = 80;
+            newPlayer.Skill.Blood = 100;
+            Derd.Model("Player").SetValues(newPlayer).Save();
+
+            dynamic obj = Derd.Model("Player").Equals("Type", 0).QueryFirst();
+            Assert.IsNotNull(obj);
+            Assert.AreEqual(0, obj.Type);
+            Assert.AreEqual(1, obj.Skill.Walk);
+            Assert.AreEqual(100, obj.Skill.Blood);
+        }
+
+        [TestCleanup]
+        public void Uninit()
+        {
+            Derd.RemoveTables();
+        }
+    }
+}
