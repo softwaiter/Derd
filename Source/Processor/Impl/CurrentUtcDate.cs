7f93c30b669ce6c97336a0a0ee107e9ae8bf80ab
softwaiter <softwaiter@126.com>
2026-05-03 15:00:12 +0800

    :construction:增加UTC时间处理器

---
+++ Source/Processor/Impl/CurrentUtcDate.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CodeM.Common.Orm.Processors
+{
+    public class CurrentUtcDate : IPropertyProcessor
+    {
+        public object Process(Model modelDefine, string propName, dynamic propValue)
+        {
+            return DateTime.UtcNow.ToString("yyyy-MM-dd");
+        }
+    }
+}
---
+++ Source/Processor/Impl/CurrentUtcTime.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CodeM.Common.Orm.Processors
+{
+    public class CurrentUtcTime : IPropertyProcessor
+    {
+        public object Process(Model modelDeine, string propName, dynamic propValue)
+        {
+            return DateTime.UtcNow.ToString("HH:mm:ss");
+        }
+    }
+}
